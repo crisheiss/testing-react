750efa1ae2c5650ce83f29d0cff56c6c8765e435
crisheiss <cristobal.heiss@joingsg.com>
2021-07-01 20:44:50 +0200

    using mount for COmmentBox component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+// This is good to avoid repeating common setups
+let wrapped
+beforeEach(() => {
+    wrapped = shallow(<App />);
+})
 
 it('shows a comment box', () => {
-    const wrapped = shallow(<App />);
     // Find returns an array with each instance found
     expect(wrapped.find(CommentBox).length).toEqual(1)
+});
+
+it('shows a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1)
 });
