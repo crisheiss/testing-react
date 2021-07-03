41d2098e17fffe54f5c66bffb53b492929b6ded4
Ceheiss <cristobal.heiss.j@gmail.com>
2021-07-03 13:18:16 +0200

    add describe in the text area test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,12 +16,24 @@ it('has a text area and a button', () => {
     expect(wrapped.find("button").length).toEqual(1)
 });
 
-it('has a text area that users can type in', () => {
-    // The object that we pass replaces the event object.
-    // Is a "controlled" event if you will
-    wrapped.find("textarea").simulate('change', {
-        target: { value: 'new comment' }
+describe('the text area', () => {
+    beforeEach(() => {
+        // The object that we pass replaces the event object.
+        // Is a "controlled" event if you will
+        wrapped.find("textarea").simulate('change', {
+            target: { value: 'new comment' }
+        });
+        wrapped.update();
     });
-    wrapped.update();
-    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
-});
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+    });
+    
+    it('text area clears after submit', () => {
+        expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+        // Submit the form
+        wrapped.find("form").simulate('submit');
+        wrapped.update();
+        expect(wrapped.find("textarea").prop("value")).toEqual("");
+    });
+})
