96736ad6f12f077876b1e38fce6a46a69e786014
crisheiss <cristobal.heiss@joingsg.com>
2021-07-08 15:21:05 +0200

    finish middleware section

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import CommentBox from 'components/CommentBox';
-import CommentList from 'components/CommentList';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 import * as actions from "actions";
 
-
 class App extends Component {
-    renderButton() {
-        if(this.props.auth) {
-            return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
-        } else {
-            return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
-        }
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      );
     }
+  }
 
-    renderHeader() {
-        return (
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/post">Post a comment</Link>
-                </li>
-                <li>
-                    {this.renderButton()}
-                </li>
-            </ul>
-        )
-    }
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                {this.renderHeader()}
-                <Route path="/post" component={CommentBox} />
-                <Route path="/" exact component={CommentList} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        auth: state.auth
-    }
-}
+  return {
+    auth: state.auth,
+  };
+};
 
 export default connect(mapStateToProps, actions)(App);
